1901aeb29356321f2cfefcb798a1f2e6f6f3f09f
DrK-ode <mailtokaj@gmail.com>
2026-01-11 20:56:43 +0100

    togglable fps overlay

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -1,9 +1,9 @@
-use bevy::dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin, FrameTimeGraphConfig};
-use bevy::input::common_conditions::input_toggle_active;
-use bevy::prelude::*;
-use bevy::text::FontSmoothing;
-use bevy_inspector_egui::bevy_egui::EguiPlugin;
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy::{
+    dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin, FrameTimeGraphConfig},
+    input::common_conditions::input_toggle_active,
+    prelude::*,
+};
+use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 
 use crate::game::scenes::GameScene;
 
@@ -12,34 +12,28 @@ pub struct DevToolsPlugin;
 
 impl Plugin for DevToolsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(FpsOverlayPlugin {
-            config: FpsOverlayConfig {
-                text_config: TextFont {
-                    font_size: 32.0,
-                    font: default(),
-                    font_smoothing: FontSmoothing::AntiAliased,
-                    ..default()
-                },
-                text_color: Color::WHITE,
-                refresh_interval: core::time::Duration::from_millis(100),
-                enabled: true,
-                frame_time_graph_config: FrameTimeGraphConfig {
+        app.add_plugins((
+            FpsOverlayPlugin {
+                config: FpsOverlayConfig {
                     enabled: false,
-                    min_fps: 30.0,
-                    target_fps: 60.0,
+                    frame_time_graph_config: FrameTimeGraphConfig {
+                        enabled:    false,
+                        min_fps:    30.,
+                        target_fps: 60.0,
+                    },
+                    ..Default::default()
                 },
             },
-        })
-        .add_plugins(EguiPlugin::default())
-        .add_plugins(
+            EguiPlugin::default(),
             WorldInspectorPlugin::new().run_if(input_toggle_active(false, KeyCode::KeyI)),
-        )
+        ))
         .add_systems(
             Update,
             (
+                customize_fps_overlay,
                 log_state_change_request.run_if(resource_changed::<NextState<GameScene>>),
                 log_state_change.run_if(state_changed::<GameScene>),
-            )
+            ),
         );
     }
 }
@@ -55,4 +49,13 @@ pub fn log_state_change_request(state: Res<NextState<GameScene>>) {
 
 pub fn log_state_change(state: Res<State<GameScene>>) {
     info!("State changed to {:#?}", **state);
+}
+
+fn customize_fps_overlay(input: Res<ButtonInput<KeyCode>>, mut overlay: ResMut<FpsOverlayConfig>) {
+    if input.just_pressed(KeyCode::KeyF) {
+        overlay.enabled = !overlay.enabled;
+    }
+    if input.just_released(KeyCode::KeyG) {
+        overlay.frame_time_graph_config.enabled = !overlay.frame_time_graph_config.enabled;
+    }
 }
